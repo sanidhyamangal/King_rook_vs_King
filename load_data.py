2a3515d44358dea52ba7ae0c3fcaa849cb941615
Sanidhya Mangal <mangalsanidhya19@gmail.com>
2019-08-19 02:03:56 +0530

    reformated: python files

--- load_data.py
+++ load_data.py
@@ -1,4 +1,4 @@
-# load pandas for data preprocessing 
+# load pandas for data preprocessing
 import pandas as pd
 
 
@@ -6,15 +6,16 @@ import pandas as pd
 class LoadData(object):
     def __init__(self, file_loc):
         self.file_loc = file_loc
-    
+
     def __load_seperate_data(self):
         # load data using pd dataframe
         data = pd.read_csv(self.file_loc)
 
-        features = data[data.columns[:-1]] # seperate features
-        labels = data[data.columns[-1]] # seperate labels
+        features = data[data.columns[:-1]]  # seperate features
+        labels = data[data.columns[-1]]  # seperate labels
 
         return features, labels
+
     def load_processed_data(self):
         # seperate_data into features and labels
         features, labels = self.__load_seperate_data()
@@ -23,10 +24,14 @@ class LoadData(object):
         features_with_dummies = pd.get_dummies(features)
 
         # onehot encode labels
-        labels_with_dummies = pd.get_dummies(labels, prefix='', prefix_sep='')
+        labels_with_dummies = pd.get_dummies(labels, prefix="", prefix_sep="")
 
         # list the root_labels
         root_labels = labels_with_dummies.columns
 
         # return set of labels
-        return {'features':features_with_dummies, 'labels':labels_with_dummies, 'root_labels': root_labels}
+        return {
+            "features": features_with_dummies,
+            "labels": labels_with_dummies,
+            "root_labels": root_labels,
+        }
--- ml_classify.py
+++ ml_classify.py
@@ -1,11 +1,10 @@
-from load_data import LoadData # data preprocessor class
-from sklearn.model_selection import train_test_split # to split dataset
+from load_data import LoadData  # data preprocessor class
+from sklearn.model_selection import train_test_split  # to split dataset
 
 # load some model for prediction task
-from sklearn.tree import DecisionTreeClassifier # decission tree classifier
-from sklearn.naive_bayes import GaussianNB # gausssian naive bayes classifier
-from sklearn.neighbors import KNeighborsClassifier # knn classifier
-from sklearn.ensemble import RandomForestClassifier # rf classifer
+from sklearn.tree import DecisionTreeClassifier  # decission tree classifier
+from sklearn.neighbors import KNeighborsClassifier  # knn classifier
+from sklearn.ensemble import RandomForestClassifier  # rf classifer
 
 # import f1 score matrix
 from sklearn.metrics import f1_score
@@ -15,8 +14,9 @@ import pickle
 
 # helper function to save the model
 def save_model(learner, file_name):
-    with open(file_name + '.pkl', 'wb') as fp:
+    with open(file_name + ".pkl", "wb") as fp:
         pickle.dump(learner, fp)
+
 
 # helper function to train and predict
 def train_predict(learner, X_train, y_train, X_test, y_test):
@@ -26,34 +26,36 @@ def train_predict(learner, X_train, y_train, X_test, y_test):
     learner = learner
 
     # print training model
-    print("Training model {}". format(learner.__class__.__name__))
+    print("Training model {}".format(learner.__class__.__name__))
     learner.fit(X_train, y_train)
 
     # print sabing the model
-    print("Saving model {}". format(learner.__class__.__name__))
+    print("Saving model {}".format(learner.__class__.__name__))
     save_model(learner, learner.__class__.__name__)
-    
+
     # predict model
     print("Predicting Model")
     prediction_test = learner.predict(X_test)
 
-    results['accu'] = f1_score(y_test, prediction_test, average='micro')
+    results["accu"] = f1_score(y_test, prediction_test, average="micro")
 
     return results
 
-# load data
-dataLoader = LoadData('./kr-vs-k.csv') # data loader for kr-vs-k
 
-data = dataLoader.load_processed_data() # load preprocessed data
+# load data
+dataLoader = LoadData("./kr-vs-k.csv")  # data loader for kr-vs-k
+
+data = dataLoader.load_processed_data()  # load preprocessed data
 
 # split data
-X_train, X_test, y_train, y_test = train_test_split(data['features'], data['labels'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    data["features"], data["labels"], test_size=0.2, random_state=42
+)
 
 # initalize classifiers
-clf_DT = DecisionTreeClassifier() # decission tree classifier
-clf_NB = GaussianNB() # GaussianNB classifier
-clf_KNN = KNeighborsClassifier(n_neighbors=4) # KNN classifier
-clf_RF = RandomForestClassifier(n_estimators=1000) # Random Forest with 100 trees
+clf_DT = DecisionTreeClassifier()  # decission tree classifier
+clf_KNN = KNeighborsClassifier(n_neighbors=4)  # KNN classifier
+clf_RF = RandomForestClassifier(n_estimators=1000)  # Random Forest with 100 trees
 
 # list of classifiers to iterate
 classifier_list = [clf_DT, clf_KNN, clf_RF]
@@ -67,7 +69,7 @@ for clf in classifier_list:
 
 
 # save results into pickle file
-with open('results.pkl', 'wb') as fp:
+with open("results.pkl", "wb") as fp:
     pickle.dump(results, fp)
 
 print("Results: \n", results)
